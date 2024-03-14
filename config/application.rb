f6d3aad298d48f7f54fdefed354546b399c3eb4f
AndrewBKang <andrew.b.kang@gmail.com>
2024-03-14 10:18:48 -0700

    Add rspec (#5)
    * create custom categories
    
    * export by category
    
    * add rspec and first spec

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Blueprints
     config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
-    #
+    config.generators { |g| g.test_framework :rspec }
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
