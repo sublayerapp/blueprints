7240e73db22a8dea13a67603708ca68fb1b39302
Andrew Kang <andrew.b.kang@gmail.com>
2024-04-10 15:30:29 -0700

    add promptability checks to performance spec

--- spec_performance/llms/llm_performance_spec.rb
+++ spec_performance/llms/llm_performance_spec.rb
@@ -1,13 +1,12 @@
 describe Api::V1::BlueprintVariantsController, type: :controller do
   let!(:blueprint) do
     create(:blueprint,
-      name: 'ChessRookPieceClass',
-      description: "The code defines a Rook class, which inherits from the Piece class. It represents a rook piece in a chess game. It includes methods to represent the rook symbol using Unicode character '\\u265C' and to generate all possible rook moves on a chessboard based on straight line movements. The class includes a private straight_moves method that calculates all valid moves considering the chessboard layout and the presence of other pieces. This method checks for moves in four cardinal directions, adds empty squares to the valid moves, and captures if the square is occupied by an opposing piece, halting further movement in that direction if any square is either invalid or occupied by a same-color piece.",
-      code: "class Rook < Piece\n  def symbol\n    \"\\u265C\"\n  end\n\n  def moves\n    straight_moves\n  end\n\n  private\n\n  def straight_moves\n    moves = []\n    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]\n\n    directions.each do |dx, dy|\n      current_x, current_y = pos\n\n      while true\n        current_x += dx\n        current_y += dy\n        current_pos = [current_x, current_y]\n\n        break unless board.valid_pos?(current_pos)\n\n        if board.empty?(current_pos)\n          moves << current_pos\n        else\n          moves << current_pos if board[current_pos].color != color\n          break\n        end\n      end\n    end\n\n    moves\n  end\nend"
+      name: 'ChessRookClass',
+      description: "A Ruby Rook class for chess",
+      code: "class Rook < Piece\n  def symbol\n    \"\u265C\"\n  end\n  def moves\n    straight_moves\n  end\n  private\n  def straight_moves\n    moves = []\n    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]\n    directions.each do |dx, dy|\n      x, y = pos\n      while true\n        x += dx\n        y += dy\n        break unless board.valid_pos?(pos)\n        if board.empty?(pos)\n          moves << pos\n        else\n          moves << pos if board[pos].color != color\n          break\n        end\n      end\n    end\n    moves\n  end\nend\n"
     )
   end
-  let(:description) { 'ruby knight class for chess' }
-
+  let(:description) { 'A Knight class for chess' }
   let(:attempts) { 1 }
 
   it 'attempts to get variant' do
@@ -32,6 +31,7 @@ describe Api::V1::BlueprintVariantsController, type: :controller do
   def run_performance_test(folder_name, attempts, is_local)
     times = []
     codes = []
+    quality_checks = []
     CSV.open("spec_performance/llms/results/#{folder_name}/results.csv", 'w') do |csv|
       attempts.times do
         time0 = Time.now.to_i
@@ -40,6 +40,7 @@ describe Api::V1::BlueprintVariantsController, type: :controller do
           elapsed_time = Time.now.to_i - time0
 
           generated_code = JSON.parse(response.body)['result']
+          quality_checks << generated_code_works?(generated_code)
           csv << [response.code, elapsed_time, generated_code, response.body]
 
           times << elapsed_time
@@ -59,6 +60,7 @@ describe Api::V1::BlueprintVariantsController, type: :controller do
       puts "times: #{times.map {|t| "#{t}s"}.join(', ')}"
       puts "Average time: #{average_time}"
       puts "Success: #{codes.count("200")} / #{codes.size}"
+      puts "Quality Checks: #{quality_checks.count(true)} / #{quality_checks.size}"
 
       "#{average_time}seconds_#{codes.count('200')}successes"
     end
@@ -78,5 +80,35 @@ describe Api::V1::BlueprintVariantsController, type: :controller do
   def clean_up_local_model(pid)
     Process.kill('TERM', pid)
     sleep(2) while system('nc -z localhost 8080 > /dev/null 2>&1')
+  end
+
+  def generated_code_works?(generated_code)
+    restricted_context = binding # protect from eval attempts to access the outside world
+    piece_class = "class Piece\n  attr_reader :color, :board\n  attr_accessor :pos\n  def initialize(color, board, pos)\n    @color, @board, @pos = color, board, pos\n    board[pos] = self\n  end\n  def inspect\n    { color: color, pos: pos, symbol: symbol }.inspect\n  end\n  def to_s\n    \" \#{symbol.colorize(color: colorize_color)} \"\n  end\n  def empty?\n    false\n  end\n  def valid_moves\n    moves.reject { |end_pos| move_into_check?(end_pos) }\n  end\n  def move_into_check?(end_pos)\n    test_board = board.dup\n    test_board.move_piece!(pos, end_pos)\n    test_board.in_check?(color)\n  end\n  def colorize_color\n    color == :white ? :light_white : :black\n  end\nend\n"
+    board_class = "class Board\n  def initialize(fill_board = true)\n    @rows = Array.new(8) { Array.new(8) }\n  end\n  def [](pos)\n    row, col = pos\n    @rows[row][col]\n  end\n  def []=(pos, piece)\n    row, col = pos\n    @rows[row][col] = piece\n  end\n  def checkmate?(color)\n    return false unless in_check?(color)\n    pieces.select { |piece| piece.color == color }.all? do |piece|\n      piece.valid_moves.empty?\n    end\n  end\n  def move_piece(turn_color, start_pos, end_pos)\n    raise \"No piece at start position\" if self[start_pos].nil?\n    raise \"Piece cannot move like that\" unless self[start_pos].moves.include?(end_pos)\n    raise \"You cannot move into check\" unless self[start_pos].valid_moves.include?(end_pos)\n    move_piece!(start_pos, end_pos)\n  end\n  def move_piece!(start_pos, end_pos)\n    piece = self[start_pos]\n    raise \"Piece cannot move like that\" unless piece.moves.include?(end_pos)\n    self[end_pos] = piece\n    self[start_pos] = nil\n    piece.pos = end_pos\n  end\n  def in_check?(color)\n    king_pos = find_king(color).pos\n    pieces.any? { |piece| piece.color != color && piece.moves.include?(king_pos) }\n  end\n  def pieces\n    @rows.flatten.compact\n  end\n  def dup\n    new_board = Board.new(false)\n    pieces.each do |piece|\n      piece.class.new(piece.color, new_board, piece.pos)\n    end\n    new_board\n  end\n  def find_king(color)\n    pieces.find { |piece| piece.color == color && piece.is_a?(King) }\n  end\n  def valid_pos?(pos)\n    pos.all? { |coord| coord.between?(0, 7) }\n  end\n  def empty?(pos)\n    self[pos].nil?\n  end\nend"
+
+    works = 0
+    begin
+      eval(piece_class, restricted_context)
+      eval(board_class, restricted_context)
+      eval(generated_code, restricted_context)
+      board_class = restricted_context.eval('Board')
+      knight_class = restricted_context.eval('Knight')
+      board = board_class.new
+
+      return true if run_test(knight_class, board)
+    rescue StandardError, SyntaxError => e
+      p e
+      return false
+    end
+  end
+
+  def run_test(knight_class, board)
+    knight = knight_class.new(:white, board, [4,4])
+    knight.symbol == "\u265D" || knight.symbol == "\u2657"
+    knight = knight_class.new(:white, board, [4,4])
+    correct_knight_moves = [[2, 3], [2, 5], [3, 2], [3, 6], [5, 2], [5, 6], [6, 3], [6, 5]]
+
+    knight.moves.sort == correct_knight_moves
   end
 end
